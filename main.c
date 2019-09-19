883c4a9db941dd738de5ea76ced0b4aa6fd80a1f
gaheunjem <gaheunjem@sookmyung.ac.kr>
2019-09-19 15:31:55 +0900

    2번 실습

--- main.c
+++ main.c
@@ -4,6 +4,21 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("hello!\n");
+	
+	int x;
+	short y;
+	
+	float a; 
+	double r;
+	
+	printf("bytes of int : %d\n", sizeof(x));
+
+	printf("bytes of short : %d\n", sizeof(y));
+	
+	
+	
+	printf("bytes of float : %d\n", sizeof(a));
+	
+	printf("bytes of double : %d\n", sizeof(r));
 	return 0;
 }
